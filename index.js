7d575fbb60212d59197b099a6819c662fef420a5
tunguyenhd <tunguyenhd01@gmail.com>
2024-12-17 21:29:10 +0700

    fjs

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ let audio = document.querySelector("audio");
 let label = document.querySelector("label");
 
 label.addEventListener("click", () => {
-   if (audio.paused) {
-      audio.play();
-   } else {
+   if (!audio.paused) {
       audio.pause();
+   } else {
+      audio.play();
    }
 });
