4e67869821ecf1e3c3e5e44e88e8714cc16659df
tunguyenhd <tunguyenhd01@gmail.com>
2024-12-18 00:50:45 +0700

    1111

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 let audio = document.querySelector("audio");
 let label = document.querySelector("label");
+let nav = document.querySelector(".nav");
+let pause = nav.querySelector(".pause");
+let play = nav.querySelector(".play");
+
+// if (audio.paused) {
+//    audio.play();
+//    pause.style.opacity = "1";
+//    play.style.opacity = "0";
+//    console.log("play");
+// }
 
 label.addEventListener("click", () => {
-   if (!audio.paused) {
-      audio.pause();
-   } else {
+   if (audio.paused) {
       audio.play();
+      pause.style.opacity = "1";
+      play.style.opacity = "0";
+      console.log("play");
+   } else {
+      audio.pause();
+      pause.style.opacity = "0";
+      play.style.opacity = "1";
+      console.log("pause");
    }
 });
